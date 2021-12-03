1c5b67f951f3268254bb1f136c2ccbfb382f8dd3
VinneyJ <vincentjayden49@gmail.com>
2021-12-04 01:13:50 +0300

    update log resolution finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) {
-    return Promise.resolve({
-      status: 200,
-      body: 'Success',
-    }).then(() => {
-      console.log('Got a response from the API');
-    });
-  }
-  return Promise.reject(new Error());
+  return Promise.resolve({
+    status: 200,
+    body: 'Success',
+  }).catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'))
 }
